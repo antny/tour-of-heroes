f5f7462b16bfaf0a966f0b7cac66692cb23f2baf
antny <antnyuk@gmail.com>
2018-02-10 20:38:56 +0000

    - add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from './in-memory-data.service';
+import {AppNavbarComponent} from './app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import {InMemoryDataService} from './in-memory-data.service';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
